396d27f1e6c9661caf58f9e62708dbef9317d879
Kay <kay.s.schwenk@gmail.com>
2025-02-28 16:59:15 -0500

    only let it happen in town square

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ async def on_message(message):
         return
 
     # Put someone in the stocks
-    if 'Guards! Seize ' in message.content:
+    if str(message.channel) == 'town-square' and 'Guards! Seize ' in message.content:
         guild = message.author.guild
         target = message.content.split(' ')[2]
         target_user = next(x for x in guild.members if str(x.id) in target)
@@ -44,7 +44,7 @@ async def on_message(message):
         await target_user.add_roles(tomato)
 
     # Remove someone from the stocks
-    if 'Guards! Release ' in message.content:
+    if str(message.channel) == 'town-square' and 'Guards! Release ' in message.content:
         guild = message.author.guild
         target = message.content.split(' ')[2]
         target_user = next(x for x in guild.members if str(x.id) in target)
