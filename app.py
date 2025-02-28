e08e24ac6707bf01e2618fc8a0c55e5889e09c1c
Kay <kay.s.schwenk@gmail.com>
2025-02-28 14:36:20 -0500

    Test tomatoes

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ sleve = '266363683741892619'
 eli = '387042210106966016'
 amelia = '135402844567240704'
 
-clark = get(client.emojis, name='hubris')
-
 def increment_hex(color):
     return discord.Color(color.value + 1)
+
+def increment_red(color):
+    return discord.Color.from_rgb(color.r + 1, 0, 0)
+
+def last_role(roles):
+    return roles[len(roles) - 1]
 
 @client.event
 async def on_message(message):
@@ -24,22 +28,41 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if str(message.channel) == 'mod-chat' and '/roles' in message.content:
-        roles = message.author.roles
-        role = roles[len(roles) - 2]
-        print(role)
-        await message.add_reaction(clark)
+    # Put someone in the stocks
+    if last_role(message.author.roles) == 'Moderator' and 'Guards! Seize ' in message.content:
+        guild = message.author.guild
+        target = message.content.split(' ')[2]
+        target_user = next(x for x in guild.members if x.id == target)
+        role = next(x for x in guild.roles if x == 'In The Stocks')
+        tomato = next(x for x in guild.roles if x == 'tomatoed')
+        for member in role.members:
+            await member.remove_roles(role)
+            await member.remove_roles(tomato)
+        await target_user.add_roles(role)
+        await target_user.add_roles(tomato)
 
-    # if str(message.channel) == 'town-square' and '!tomato' in message.content:
+    # Remove someone from the stocks
+    if last_role(message.author.roles) == 'Moderator' and 'Guards! Release ' in message.content:
+        guild = message.author.guild
+        target = message.content.split(' ')[2]
+        target_user = next(x for x in guild.members if x.id == target)
+        role = next(x for x in guild.roles if x == 'In The Stocks')
+        await target_user.remove_roles(role)
 
+    # Throw a tomato at someone in the stocks
+    if str(message.channel) == 'town-square' and '!tomato' in message.content:
+        guild = message.author.guild
+        tomato = next(x for x in guild.roles if x == 'tomatoed')
+        color = increment_red(tomato.color)
+        await tomato.edit(color=color)
+
+    # Amelia color increment on message
     if str(message.author.id) == amelia:
-        roles = message.author.roles
-        role = roles[len(roles) - 1]
+        role = last_role(message.author.roles)
         if str(role.color) != '#ffffff':
             color = increment_hex(role.color)
             print('Color updated from ' + str(role.color) + ' to ' + str(color))
             await role.edit(color=color)
-
 
 @client.event
 async def on_ready():
